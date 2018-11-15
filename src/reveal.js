73c9627047bf5549458c54c524368440f8642579
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-11-16 09:23:46 +1000

    Initial commit and creation of babel & minification

---
+++ src/reveal.js
@@ -0,0 +1,43 @@
+const reveal = () => {
+    const revealList = Array.from(document.querySelectorAll(".reveal"));
+    console.log(revealList);
+
+    const buffer = 50;
+
+    const greaterThan = (a, b) => a > b;
+
+    const rollIn = ({ container }) => {
+      const colorlayer = container.querySelector(".colorlayer");
+
+      const contentlayer = container.querySelector(".contentlayer");
+
+      container.classList.add("visible");
+
+      colorlayer.classList.add("visible");
+
+      wait(400).then(() => {
+        contentlayer.classList.add("visible");
+      });
+    };
+
+    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+    const visibleOnPage = element => {
+      console.log(element);
+      const X = element.offsetLeft + element.style.width;
+      const Y = element.offsetTop + element.clientHeight + buffer;
+      const scrollY = window.scrollY + document.body.clientHeight;
+      const scrollX = window.scrollX + window.outerWidth;
+      console.log(`X: ${X}, Y: ${Y}, scrollX: ${scrollX}, scrollY: ${scrollY}`);
+      return greaterThan(scrollY, Y);
+    };
+
+    window.addEventListener("scroll", () => {
+      const action = container =>
+        visibleOnPage(container)
+          ? rollIn({ container })
+          : console.log("not Visible");
+
+      revealList.forEach(element => action(element));
+    });
+  };
