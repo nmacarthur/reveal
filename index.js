796cec2892de7018f905392a037199442fd9c79e
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-05-11 15:53:02 +1000

    rebuild begin

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const Reveal = require('./lib/reveal.js');
 
-export default Reveal;
+import { ElementObserver }  from 'viewprt';
+
+const Reveal = () => {
+    const revealList = Array.from(document.querySelectorAll(".reveal"));
+
+    revealList.forEach(element => {
+      console.log(element);
+      const observer = new ElementObserver(element, {
+        
+      })
+    })
+
+  };
+
+  export { Reveal };
